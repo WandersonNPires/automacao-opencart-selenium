1560117394067e0a9aeee1cecdbfebfdb3ec962c
Wanderson Neiva <wanderson.silva@base2.com.br>
2025-07-03 19:25:55 -0300

    Adicionado Teste Pagina  editar senha da  Conta

---
+++ src/test/java/test/EditAccountTeste.java
@@ -0,0 +1,55 @@
+package test;
+
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+import Checks.EditCheck;
+import data.MassOfData;
+import pages.EditAccountPage;
+import pages.LoginPage;
+
+public class EditAccountTeste {
+
+    @BeforeClass
+    public void prepararBanco() {
+        MassOfData.prepararMassa(); 
+    }
+
+    @Test
+    public void editarConta() throws InterruptedException {
+        ChromeDriver navegador = new ChromeDriver();
+        navegador.manage().window().maximize();
+
+       
+            LoginPage loginPage = new LoginPage(navegador);
+
+            String emailAddress = MassOfData.EMAIL_VALIDO_4;
+            String password = MassOfData.SENHA_VALIDA_4;
+            String expectedMessage = "Success: Your password has been successfully updated.";
+
+            loginPage.acessarAplicacaoWeb();
+            loginPage.inserirEmailLogin(emailAddress);
+            loginPage.inserirPassWordLogin(password);
+            loginPage.logar();
+
+            EditAccountPage editAccountPage = new EditAccountPage(navegador);
+
+            editAccountPage.acessarTelaMudarSenha();
+            editAccountPage.clicarEmTrocar();
+            String novaSenha = "senhanova";
+            editAccountPage.inserirNovaSenha(novaSenha);
+            editAccountPage.inserirConfirmacaoSenha(novaSenha);
+            editAccountPage.clicarParaTrocar();
+
+            EditCheck trocaFeita = new EditCheck(navegador);
+            String mensagemEsperada = trocaFeita.mudarSenhaSucesso();
+            Assert.assertEquals(mensagemEsperada, expectedMessage);
+
+
+
+        } 
+        }
+
+
