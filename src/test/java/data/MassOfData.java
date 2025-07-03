b00198d3e1e5cea8fe0d674cd6260463b4e2574d
Wanderson Neiva <wanderson.silva@base2.com.br>
2025-07-03 15:53:00 -0300

    Modificação Massa de Dados

--- src/test/java/data/MassOfData.java
+++ src/test/java/data/MassOfData.java
@@ -7,7 +7,36 @@ import java.net.URL;
 public class MassOfData {
 
     
+  public static final String FIRST_NAME_VALIDO = "Wanderson";
+    public static final String LAST_NAME_VALIDO = "Silva";
+    public static final String EMAIL_VALIDO = "wandersonneiva@gmail.com";
+    public static final String SENHA_VALIDA = "eumesmo123";
 
+    public static final String FIRST_NAME_VALIDO_2 = "Marilene";
+    public static final String LAST_NAME_VALIDO_2 = "Lourenço";
+    public static final String EMAIL_VALIDO_2 = "marilene.lourenco@base2.com.br";
+    public static final String SENHA_VALIDA_2 = "senha123";
+
+    public static final String FIRST_NAME_VALIDO_3 = "Maria";
+    public static final String LAST_NAME_VALIDO_3 = "Santos";
+    public static final String EMAIL_VALIDO_3 = "maria@example.com";
+    public static final String SENHA_VALIDA_3 = "senha4senha123";
+
+    public static final String FIRST_NAME_VALIDO_4 = "WandersonN";
+    public static final String LAST_NAME_VALIDO_4 = "SilvaP";
+    public static final String EMAIL_VALIDO_4 = "wandersonneivaa@gmail.com";
+    public static final String SENHA_VALIDA_4 = "eumesmo12";
+
+
+
+    public static final String FIRST_NAME_VALIDO_5 = "NovousuarioTeste";
+    public static final String LAST_NAME_VALIDO_5 = "Silva";
+    public static final String EMAIL_VALIDO_5 = "wandersonneivaQA@gmail.com";
+    public static final String SENHA_VALIDA_5 = "eumesmo12";
+
+
+    public static final String Email_Invalido ="wandersonnnnnn@gmail.com";
+    public static final String Senha_Ivalida = "eumesmo12";
 
 
     // Método para chamar o script PHP e criar a massa de dados no banco
