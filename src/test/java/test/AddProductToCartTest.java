ee1873af70072e06c7733af2dc7e191c2697dc6b
Wanderson Neiva <wanderson.silva@base2.com.br>
2025-07-21 13:29:17 -0300

    Adicinado Print e Relatorio na classe AddProductCart

--- src/test/java/test/AddProductToCartTest.java
+++ src/test/java/test/AddProductToCartTest.java
@@ -11,15 +11,17 @@ import Checks.AddProductToCartCheck;
 import data.MassOfData;
 import pages.AddProductToCartPage;
 import pages.LoginPage;
+import utils.Report;
 
 public class AddProductToCartTest {
     ChromeDriver navegador;
+
     @BeforeClass
     public void prepararBanco() {
         MassOfData.prepararMassa();
     }
 
-      @BeforeMethod
+    @BeforeMethod
     public void setup() {
         navegador = new ChromeDriver();
         navegador.manage().window().maximize();
@@ -33,7 +35,7 @@ public class AddProductToCartTest {
 
     @Test
     public void adicionarProdutoNoCarrinho() {
-        
+
         AddProductToCartPage addProductToCartPage = new AddProductToCartPage(navegador);
 
         addProductToCartPage.acessarAplicacaoWeb();
@@ -49,7 +51,7 @@ public class AddProductToCartTest {
 
         AddProductToCartCheck validarQuatidadeCarrinho = new AddProductToCartCheck(navegador);
         String mensssagemEsperada = validarQuatidadeCarrinho.validarQuantidadeNoCarrinho();
-        String expextedMessage2 ="x 1";
+        String expextedMessage2 = "x 1";
         Assert.assertEquals(mensssagemEsperada, expextedMessage2);
 
         addProductToCartPage.clicarNoCarrinhoDeCompra();
@@ -63,13 +65,21 @@ public class AddProductToCartTest {
 
         AddProductToCartCheck validarQuatidadeCarrinho2 = new AddProductToCartCheck(navegador);
         String mensssagemEsperada2 = validarQuatidadeCarrinho2.validarQuantidadeNoCarrinho();
-        String expextedMessage3 ="x 2";
+        String expextedMessage3 = "x 2";
         Assert.assertEquals(mensssagemEsperada2, expextedMessage3);
-        
+
+        String nomePasta = "AddProductDocuments";
+        String nomeTeste = "adicionarProdutoNoCarrinho";
+
+        Report.tirarScreenshot(navegador, nomePasta, nomeTeste);
+        Report.gerarRelatorioPorTeste(nomePasta, nomeTeste);
+
+        navegador.quit();
 
     }
+
     @Test
-    public void removerProduto(){
+    public void removerProduto() {
 
         AddProductToCartPage addProductToCartPage = new AddProductToCartPage(navegador);
 
@@ -81,7 +91,7 @@ public class AddProductToCartTest {
 
         AddProductToCartCheck validarRemocaoItem = new AddProductToCartCheck(navegador);
         String mensagemEsperada = validarRemocaoItem.validarMensagemdeRemocaodeItem();
-        String expextedMessage= "Success: You have removed an item from your shopping cart!";
+        String expextedMessage = "Success: You have removed an item from your shopping cart!";
         Assert.assertEquals(mensagemEsperada, expextedMessage);
 
         AddProductToCartCheck validarRemocao = new AddProductToCartCheck(navegador);
@@ -89,13 +99,22 @@ public class AddProductToCartTest {
         String expectedMessage2 = "Your shopping cart is empty!";
         Assert.assertEquals(mensagemEsperada2, expectedMessage2);
 
+        String nomePasta = "AddProductDocuments";
+        String nomeTeste = "removerProduto";
+
+        Report.tirarScreenshot(navegador, nomePasta, nomeTeste);
+        Report.gerarRelatorioPorTeste(nomePasta, nomeTeste);
+
+        navegador.quit();
+
     }
+
     @Test
-    public void validarTotalCarrinho(){
+    public void validarTotalCarrinho() {
         AddProductToCartPage addProductToCartPage = new AddProductToCartPage(navegador);
         addProductToCartPage.acessarMac();
         addProductToCartPage.clicarNoCarrinhoDeCompra();
-        
+
         AddProductToCartCheck adicionaCarrinho3 = new AddProductToCartCheck(navegador);
         String menssagemEsperada3 = adicionaCarrinho3.validandoProdutoInclusoNoCarrinho();
         String expectedMessage3 = "Success: You have added iMac to your shopping cart!";
@@ -103,7 +122,7 @@ public class AddProductToCartTest {
 
         AddProductToCartCheck validarValorCarrinho = new AddProductToCartCheck(navegador);
         String mensagemEsperada = validarValorCarrinho.validarTotalCarrinho();
-        String expectedmessange="$100.00";
+        String expectedmessange = "$100.00";
         Assert.assertEquals(mensagemEsperada, expectedmessange);
 
         addProductToCartPage.clicarNoCarrinhoDeCompra();
@@ -112,16 +131,24 @@ public class AddProductToCartTest {
         String menssagemEsperada4 = adicionaCarrinho4.validandoProdutoInclusoNoCarrinho();
         String expectedMessage4 = "Success: You have added iMac to your shopping cart!";
         Assert.assertEquals(menssagemEsperada4, expectedMessage4);
-        
 
         AddProductToCartCheck validarValorCarrinho2 = new AddProductToCartCheck(navegador);
         String mensagemEsperada2 = validarValorCarrinho2.validarTotalCarrinho();
-        String expectedmessange2="$200.00";
+        String expectedmessange2 = "$200.00";
         Assert.assertEquals(mensagemEsperada2, expectedmessange2);
 
+        String nomePasta = "AddProductDocuments";
+        String nomeTeste = "validarTotalCarrinho";
+
+        Report.tirarScreenshot(navegador, nomePasta, nomeTeste);
+        Report.gerarRelatorioPorTeste(nomePasta, nomeTeste);
+
+        navegador.quit();
+
     }
+
     @Test
-    public void visualizarCarrinho(){
+    public void visualizarCarrinho() {
         AddProductToCartPage addProductToCartPage = new AddProductToCartPage(navegador);
         addProductToCartPage.acessarAplicacaoWeb();
         addProductToCartPage.acessarMac();
@@ -130,18 +157,23 @@ public class AddProductToCartTest {
         addProductToCartPage.acessarCarrinhoDeCompras();
 
         AddProductToCartCheck visualizarCarrinho = new AddProductToCartCheck(navegador);
-        String mensagemEsperada= visualizarCarrinho.visualizarCarrinho();
-        String expectedMessage ="Shopping Cart (5.00kg)";
+        String mensagemEsperada = visualizarCarrinho.visualizarCarrinho();
+        String expectedMessage = "Shopping Cart (5.00kg)";
         Assert.assertEquals(mensagemEsperada, expectedMessage);
 
         AddProductToCartCheck carrinhoComProduto = new AddProductToCartCheck(navegador);
         String mensagemEsperada2 = carrinhoComProduto.visualizarCarrinhoComProduto();
-        String expectedMessage2 ="- Model: Product 14";
+        String expectedMessage2 = "- Model: Product 14";
         Assert.assertEquals(mensagemEsperada2, expectedMessage2);
 
+        String nomePasta = "AddProductDocuments";
+        String nomeTeste = "visualizarCarrinho";
 
+        Report.tirarScreenshot(navegador, nomePasta, nomeTeste);
+        Report.gerarRelatorioPorTeste(nomePasta, nomeTeste);
 
-        
+        navegador.quit();
+
     }
 
 }
